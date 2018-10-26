72bd812b35c09ad86cd2069bacc37056cfcd5582
nilsonmolina <nilson13@me.com>
2018-10-25 23:21:49 -0700

    almost done with pie chart

--- d3-basics/04-ninja-wonga/graph.js
+++ d3-basics/04-ninja-wonga/graph.js
@@ -1,8 +1,10 @@
 /* global d3:true db:true */
+/* eslint no-param-reassign: ["error", { "props": false }] */
+/* eslint no-underscore-dangle: ["error", { "allowAfterThis": true }] */
 /*---------------------------
   VARIABLES
 ---------------------------*/
-const dims = { height: 350, width: 350, radius: 175 };
+const dims = { height: 320, width: 320, radius: 140 };
 const center = { x: (dims.width / 2 + 5), y: (dims.height / 2 + 5) };
 
 const svg = d3.select('.canvas')
@@ -23,10 +25,36 @@ const arcPath = d3.arc()
 
 const color = d3.scaleOrdinal(d3.schemeSet3);
 
+// legend setup
+const legendGroup = svg.append('g')
+  .attr('transform', `translate(${dims.width + 40}, 10)`);
+
+const legend = d3.legendColor()
+  .shape('circle')
+  .shapePadding(10)
+  .scale(color);
+
+// tooltip setup
+const tooltip = d3.tip()
+  .attr('class', 'tip card')
+  .html((d) => {
+    let content = `<div class="name">${d.data.name}</div>`;
+    content += `<div class="cost">${d.data.cost}</div>`;
+    content += '<div class="delete">Click slice to delete</div>';
+    return content;
+  });
+
+graph.call(tooltip);
+
 // update function
 const update = (data) => {
   // update color scale domain
   color.domain(data.map(d => d.name));
+
+  // update and call legend
+  legendGroup.call(legend)
+    .selectAll('text')
+    .attr('fill', 'white');
 
   // join enhanced (pie) data to path elements
   const paths = graph.selectAll('path')
@@ -40,7 +68,10 @@ const update = (data) => {
     .remove();
 
   // update the paths already in DOM
-  paths.attr('d', arcPath);
+  paths.attr('d', arcPath)
+    .transition()
+    .duration(1000)
+    .attrTween('d', arcTweenUpdate);
 
   // create virtual paths & append to DOM
   paths.enter()
@@ -49,9 +80,23 @@ const update = (data) => {
     .attr('stroke', '#fff')
     .attr('stroke-width', 3)
     .attr('fill', d => color(d.data.name))
+    .each(function f(d) { this._current = d; })
     .transition()
     .duration(1000)
     .attrTween('d', arcTweenEnter);
+
+  // add events
+  graph.selectAll('path')
+    .on('click', handleClick)
+    .on('mouseover', handleMouseOver)
+    .on('mouseout', handleMouseOut);
+  // .on('mouseover', (d, i, n) => {
+  //   tooltip.show(d, n[i]);
+  //   handleMouseOver(d, i, n);
+  // }).on('mouseout', (d, i, n) => {
+  //   tooltip.hide();
+  //   handleMouseOut(d, i, n);
+  // });
 };
 
 // data array and firestore
@@ -98,3 +143,35 @@ const arcTweenExit = (d) => {
     return arcPath(d);
   };
 };
+
+// use function keyword to allow 'this'
+function arcTweenUpdate(d) {
+  // interpolate between the two objects
+  const i = d3.interpolate(this._current, d);
+  // update the current prop with updated data
+  this._current = i(1);
+
+  return function f(t) {
+    return arcPath(i(t));
+  };
+}
+
+const handleMouseOver = (d, i, n) => {
+  d3.select(n[i])
+    .transition('changeSliceFill')
+    .duration(300)
+    // .attr('fill', '#fff');
+    .attr('transform', 'scale(1.1)');
+  tooltip.show(d, n[i]);
+};
+
+const handleMouseOut = (d, i, n) => {
+  d3.select(n[i])
+    .transition('changeSliceFill')
+    .duration(300)
+    // .attr('fill', color(d.data.name));
+    .attr('transform', 'scale(1)');
+  tooltip.hide();
+};
+
+const handleClick = (d) => { db.collection('expenses').doc(d.data.id).delete(); };
